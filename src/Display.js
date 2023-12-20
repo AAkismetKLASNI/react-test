de6d4cbcd01e245126a8603344f4f87f193d4f97
erik <forevernoox@gmail.com>
2023-12-20 14:31:27 +0500

    new

--- src/Display.js
+++ src/Display.js
@@ -26,23 +26,29 @@ export const Display = () => {
 		}
 	};
 
-	const calculationsDisplay = (item, type) => {
-		if (type === 'NUMBER') {
+	const calculationsDisplay = (event) => {
+		const isNumber = event.target.closest('button[data-btn-number]');
+		const isOperation = event.target.closest('button[data-operation]');
+		const isCommand = event.target.closest('button[data-command]');
+
+		if (isNumber) {
 			setColorResult(false);
 			if (operand1 !== '0' && operand2 !== '0') {
-				!operator ? setOperand1(operand1 + item) : setOperand2(operand2 + item);
+				!operator
+					? setOperand1(operand1 + isNumber.value)
+					: setOperand2(operand2 + isNumber.value);
 			} else {
-				!operator ? setOperand1(item) : setOperand2(item);
+				!operator ? setOperand1(isNumber.value) : setOperand2(isNumber.value);
 			}
-		} else if (type === 'OPERATION') {
+		} else if (isOperation) {
 			setColorResult(false);
-			if (!operator) {
-				setOperator(operator + item);
-			}
-		} else {
-			if (item === 'C') {
+			!operator
+				? setOperator(operator + isOperation.value)
+				: setOperator(isOperation.value);
+		} else if (isCommand) {
+			if (isCommand.value === 'C') {
 				clear();
-			} else if (item === '=') {
+			} else if (isCommand.value === '=') {
 				setColorResult(true);
 				getExpression(operator);
 			}
@@ -56,34 +62,22 @@ export const Display = () => {
 				value={operand1 + operator + operand2}
 				style={colorResult ? { color: '#FF0000' } : { color: '#000' }}
 			/>
-			<div className={styles.wrapper}>
+			<div className={styles.wrapper} onClick={calculationsDisplay}>
 				<div className={styles.containerNums}>
 					{NUMS.map((num) => (
-						<button
-							value={num}
-							key={num}
-							onClick={() => calculationsDisplay(num, 'NUMBER')}
-						>
+						<button value={num} key={num} data-btn-number={1}>
 							{num}
 						</button>
 					))}
 				</div>
 				<div className={styles.containerOperation}>
 					{OPERATIONS.map((operation) => (
-						<button
-							value={operation}
-							key={operation}
-							onClick={() => calculationsDisplay(operation, 'OPERATION')}
-						>
+						<button value={operation} key={operation} data-operation={2}>
 							{operation}
 						</button>
 					))}
 					{COMMANDS.map((command) => (
-						<button
-							value={command}
-							key={command}
-							onClick={() => calculationsDisplay(command, 'COMMAND')}
-						>
+						<button value={command} key={command} data-command={3}>
 							{command}
 						</button>
 					))}
