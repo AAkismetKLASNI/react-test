b16d0875a9bd8d3711183d3b795baf996224f3f4
erik <forevernoox@gmail.com>
2023-12-20 13:23:35 +0500

    update

--- src/MyConponent.js
+++ src/MyConponent.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { NUMS, COMMANDS, OPERATIONS } from './core/constants';
 import styles from './App.module.css';
 
 export const Display = () => {
@@ -7,51 +8,43 @@ export const Display = () => {
 	const [operator, setOperator] = useState('');
 	const [colorResult, setColorResult] = useState(false);
 
-	const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-	const OPERATIONS = ['+', '-'];
-	const COMMANDS = ['C', '='];
+	const clear = () => {
+		setOperand1('');
+		setOperand2('');
+		setOperator('');
+	};
 
-	const calculationsNums = (event) => {
-		const isNum = event.target.closest('.num');
-		const isOperation = event.target.closest('.op');
-		const isCommand = event.target.closest('.command');
+	const getExpression = (operator) => {
+		if (operator === '+') {
+			setOperand1(Number(operand1) + Number(operand2));
+			setOperand2('');
+			setOperator('');
+		} else if (operator === '-') {
+			setOperand1(Number(operand1) - Number(operand2));
+			setOperand2('');
+			setOperator('');
+		}
+	};
 
-		if (isNum) {
+	const calculationsDisplay = (item, type) => {
+		if (type === 'NUMBER') {
 			setColorResult(false);
-			if (operator.length === 0) {
-				operand1 === '0'
-					? setOperand1(isNum.value)
-					: setOperand1(operand1 + isNum.value);
+			if (operand1 !== '0' && operand2 !== '0') {
+				!operator ? setOperand1(operand1 + item) : setOperand2(operand2 + item);
 			} else {
-				operand2 === '0'
-					? setOperand2(isNum.value)
-					: setOperand2(operand2 + isNum.value);
+				!operator ? setOperand1(item) : setOperand2(item);
 			}
-		} else if (isOperation) {
+		} else if (type === 'OPERATION') {
 			setColorResult(false);
-			if (operator.length === 0) {
-				setOperator(() => operator + isOperation.value);
+			if (!operator) {
+				setOperator(operator + item);
 			}
-		} else if (isCommand) {
-			if (isCommand.value === 'C') {
-				setOperand1('');
-				setOperand2('');
-				setOperator('');
-			} else if (String(isCommand.value) === '=') {
+		} else {
+			if (item === 'C') {
+				clear();
+			} else if (item === '=') {
 				setColorResult(true);
-				if (operand1.length !== 0 || operand2.length !== 0) {
-					if (operator === '+') {
-						const result = Number(operand1) + Number(operand2);
-						setOperand1(String(result));
-						setOperand2('');
-						setOperator('');
-					} else if (operator === '-') {
-						const result = Number(operand1) - Number(operand2);
-						setOperand1(String(result));
-						setOperand2('');
-						setOperator('');
-					}
-				}
+				getExpression(operator);
 			}
 		}
 	};
@@ -63,22 +56,34 @@ export const Display = () => {
 				value={operand1 + operator + operand2}
 				style={colorResult ? { color: '#FF0000' } : { color: '#000' }}
 			/>
-			<div className={styles.wrapper} onClick={calculationsNums}>
+			<div className={styles.wrapper}>
 				<div className={styles.containerNums}>
 					{NUMS.map((num) => (
-						<button value={num} className="num" key={num}>
+						<button
+							value={num}
+							key={num}
+							onClick={() => calculationsDisplay(num, 'NUMBER')}
+						>
 							{num}
 						</button>
 					))}
 				</div>
 				<div className={styles.containerOperation}>
 					{OPERATIONS.map((operation) => (
-						<button value={operation} className="op" key={operation}>
+						<button
+							value={operation}
+							key={operation}
+							onClick={() => calculationsDisplay(operation, 'OPERATION')}
+						>
 							{operation}
 						</button>
 					))}
 					{COMMANDS.map((command) => (
-						<button value={command} className="command" key={command}>
+						<button
+							value={command}
+							key={command}
+							onClick={() => calculationsDisplay(command, 'COMMAND')}
+						>
 							{command}
 						</button>
 					))}
