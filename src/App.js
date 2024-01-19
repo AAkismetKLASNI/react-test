e7019f9a880cabe1585a9cffcdd5eb7aea6e9d5a
erik <forevernoox@gmail.com>
2024-01-19 22:38:45 +0500

    0.1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-// Императивный подход
-import logo from './logo.svg';
-import './App.css';
-import { createElement } from 'react';
-
-// Декларативный подход
-const fullYear = new Date().getFullYear();
-const codeHtmlEl = createElement('code', null, 'src/App.js');
-
+import { Form } from './components/index'
+ 
 export const App = () => {
-	return createElement(
-		'div',
-		{ className: 'App' },
-		createElement(
-			'header',
-			{ className: 'App-header' },
-			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			createElement('p', null, 'Edit ', codeHtmlEl, ' and save to reload 2.'),
-			createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			createElement('div', null, fullYear),
-		),
-	);
+	return <>
+	<Form/>
+	</>
 };
---
+++ src/components/form/Form.js
@@ -0,0 +1,12 @@
+export const Form = () => {
+	return <>
+		<form>
+			<p>Введите email</p>
+			<input type="email" />
+			<p>Введите пароль</p>
+			<input type="password" />
+			<p>Повторите пароль</p>
+			<input type="password" />
+		</form>
+	</>
+}
