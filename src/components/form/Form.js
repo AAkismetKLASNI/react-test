c64931712d9d3726e7d4a16f0cccfbfefcf75b31
erik <forevernoox@gmail.com>
2024-01-23 20:32:07 +0500

    correction

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -125,7 +125,7 @@ export const Form = () => {
 
 	const onSubmit = (event) => {
 		event.preventDefault();
-		console.log({ email, password });
+		sendFromData({ email, password });
 	};
 
 	return (
